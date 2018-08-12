874c55a1fc140b71fa432facf358fff3e18e26c2
jmoore5268 <jmoore5268@gmail.com>
2018-08-11 21:12:26 -0400

    Cleaned up test function

---
+++ Tavern.Ui/Demo/PeopleController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tavern.Ui.Demo
+{
+	[EnableQuery]
+	public class PeopleController : ODataController
+	{
+		public IActionResult Get()
+		{
+			return Ok(DemoDataSources.Instance.People.AsQueryable());
+		}
+	}
+}
